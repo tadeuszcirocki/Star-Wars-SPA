3cd4cd2ee7ea91579ad18b29602f52e97d1e8f54
Tadeusz Cirocki <tadeusz.cirocki1@gmail.com>
2019-03-27 23:15:30 +0100

    First commit

---
+++ src/Components/App.js
@@ -0,0 +1,21 @@
+import React, { Component } from 'react';
+import 'C:/Users/Tadeusz/my-app/src/Styles/App.css';
+
+class App extends Component {
+  constructor(){
+    super()
+    this.state = {
+      
+    }
+
+  }
+  render() {
+    return (
+      <div className="App-header">
+        ssss
+      </div>
+    );
+  }
+}
+
+export default App;
